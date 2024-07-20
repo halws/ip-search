36e1aa915bee079ee0418631164015e3a9e3ad95
Nazar Kotsyuba <kotsuyba@gmail.com>
2024-07-20 19:29:20 +0200

    chore: Update npm dependencies and add IP lookup functionality

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,60 @@
-<script setup lang="ts" generic="T extends any, O extends any">
+<script setup lang="ts">
+import { useAxios } from '@vueuse/integrations/useAxios'
+
 defineOptions({
   name: 'IndexPage',
 })
+
+const url = 'http://ip-api.com/batch'
+interface IpApiResponse {
+  status: string
+  country: string
+  countryCode: string
+  region: string
+  regionName: string
+  city: string
+  zip: string
+  lat: number
+  lon: number
+  timezone: string
+  isp: string
+  org: string
+  as: string
+  query: string
+}
+
+const ipAddresses = reactive([
+  { raw: '', results: ref<IpApiResponse[] | undefined>(undefined) },
+])
+
+function joinIPs(rawIPs: string) {
+  return rawIPs
+    .replace(/ /g, '')
+    .split(',')
+    .map(ip => ip.replace(/ /g, ''))
+}
+
+const { execute } = useAxios<IpApiResponse[]>(
+  url,
+  { method: 'POST' },
+  { immediate: false },
+)
+// execute({ params: { key: 1 } });
+// execute({ params: { key: 2 } });
+
+// execute()
+async function getIpInfo(ip: string, index: number) {
+  try {
+    const result = await execute({
+      data: joinIPs(ip),
+    })
+
+    ipAddresses[index].results = result.data.value
+  }
+  catch (err) {
+    console.error('err: ', err)
+  }
+}
 </script>
 
 <template>
@@ -25,7 +78,7 @@ defineOptions({
         </p>
         <!-- Add Button -->
         <div class="mb-4">
-          <button class="text-sm btn">
+          <button class="text-sm btn" @click="getIpInfo(ipAddresses[0].raw, 0)">
             <span class="mr-2 text-xl">+</span> Add
           </button>
         </div>
@@ -34,42 +87,27 @@ defineOptions({
         <hr class="my-4 h-px border-0 bg-gray-200 dark:bg-gray-700">
 
         <!-- Input Fields -->
-        <div class="mb-2 flex items-center p-2">
-          <div class="mr-2 h-10 w-10 flex items-center justify-center rounded-full bg-gray-200 text-gray-700">
+        <div
+          v-for="(item, i) in ipAddresses"
+          :key="i"
+          class="mb-2 flex items-center p-2"
+        >
+          <div
+            class="mr-2 h-10 w-10 flex items-center justify-center rounded-full bg-gray-200 text-gray-700"
+          >
             1
           </div>
-          <input type="text" class="flex-grow border rounded-sm p-2 focus:outline-none" placeholder="Enter IP address">
+          <input
+            v-model="item.raw"
+            type="text"
+            class="flex-grow border rounded-sm p-2 focus:outline-none"
+            placeholder="Enter IP address"
+            @blur="getIpInfo(item.raw, i)"
+          >
+          {{ JSON.stringify(item.results) }}
         </div>
       </div>
     </div>
-    <!-- <div i-carbon-campsite inline-block text-4xl />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      placeholder="What's your name?"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
-      </button>
-    </div> -->
   </div>
 </template>
 
